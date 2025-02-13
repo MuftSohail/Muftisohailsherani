3e3d30e54c8729b4eb6028f372617552f3eac4ea
MuftSohail <ksherani73@gmail.com>
2025-02-13 17:10:13 +0500

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert("آپ کا پیغام موصول ہو گیا ہے۔ شکریہ!");
-        form.reset();
-    });
